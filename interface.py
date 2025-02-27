5aa6ed397c67b44795a1becaa0f4d188844f08f4
Your Name <you@example.com>
2025-02-27 11:08:40 +0100

    Create authentification Windows for set an username and password for access to chat

--- interface.py
+++ interface.py
@@ -1,18 +1,51 @@
 import tkinter as tk
-from tkinter import scrolledtext
+from tkinter import scrolledtext, messagebox
+
+username = ""
+password = ""
+
+def authenticate():
+    global username, password
+    auth_window = tk.Toplevel(root)
+    auth_window.title("Authentification")
+    auth_window.geometry("300x200")
+
+    tk.Label(auth_window, text="Nom d'utilisateur:").pack(pady=5)
+    username_entry = tk.Entry(auth_window)
+    username_entry.pack(pady=5)
+
+    tk.Label(auth_window, text="Clé :" ).pack(pady=5)
+    password_entry= tk.Entry(auth_window, show='*')
+    password_entry.pack(pady=5)
+
+    def submit(user, keys):
+        global username, password
+        if user and keys:
+            username = user
+            password = keys
+            auth_window.destroy()
+        else:
+            messagebox.showerror("Erreur", "Veuillez entrer un nom d'utilisateur !")
+
+    submit_button = tk.Button(auth_window, text="Valider", command=lambda: submit(username_entry.get(), password_entry.get()))
+    submit_button.pack(pady=10)
+
+    root.wait_window(auth_window)
 
 def send_message():
+    global username, password
     message = entry_field.get()
-    if message.strip():  
-        chat_display.insert(tk.END, f"You: {message}\n")
-        entry_field.delete(0, tk.END)  
-        chat_display.yview(tk.END)  
-
+    if message.strip():
+        chat_display.insert(tk.END, f"{username}: {message}\n")
+        entry_field.delete(0, tk.END)
+        chat_display.yview(tk.END)
 
 root = tk.Tk()
 root.title("Chatbox")
 root.geometry("800x500")
 
+
+authenticate()
 
 chat_display = scrolledtext.ScrolledText(root, wrap=tk.WORD, state='normal', width=100, height=30)
 chat_display.pack(pady=10, padx=10)
@@ -24,3 +57,4 @@ send_button = tk.Button(root, text="Send", command=send_message)
 send_button.pack(pady=5)
 
 root.mainloop()
+
